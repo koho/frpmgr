cb64ee108a9034b9eb7794ce8aa5223fd9d069df
Gerhard Tan <gwohau.tan@gmail.com>
2022-04-22 15:41:06 +0800

    Bump frp version to 0.42.0

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module github.com/koho/frpmgr
 go 1.16
 
 require (
-	github.com/fatedier/frp v0.41.0
-	github.com/fatedier/golib v0.1.1-0.20220321042308-c306138b83ac
+	github.com/fatedier/frp v0.42.0
 	github.com/go-ole/go-ole v1.2.6
 	github.com/lxn/walk v0.0.0-20210112085537-c389da54e794
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e
--- services/frp.go
+++ services/frp.go
@@ -4,14 +4,9 @@ import (
 	_ "github.com/fatedier/frp/assets/frpc"
 	frpc "github.com/fatedier/frp/cmd/frpc/sub"
 	"github.com/fatedier/frp/pkg/config"
-	"github.com/fatedier/golib/crypto"
-	"math/rand"
-	"time"
 )
 
 func runFrpClient() {
-	crypto.DefaultSalt = "frp"
-	rand.Seed(time.Now().UnixNano())
 	// Change program arguments for frpc to parse
 	// No need to change it for now
 	frpc.Execute()
