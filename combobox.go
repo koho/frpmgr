8ce4b7425034ed7fa13d705791f8541ad15b6fe7
Gerhard Tan <gwohau.tan@gmail.com>
2023-07-06 11:10:48 +0800

    Trigger text changed event when selected index changed

--- combobox.go
+++ combobox.go
@@ -668,6 +668,7 @@ func (cb *ComboBox) WndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) u
 		case win.CBN_SELCHANGE:
 			cb.selChangeIndex = selIndex
 			cb.currentIndexChangedPublisher.Publish()
+			cb.textChangedPublisher.Publish()
 
 		case win.CBN_SELENDCANCEL:
 			if cb.selChangeIndex != -1 {
