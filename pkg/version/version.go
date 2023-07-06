c58976a4d404f1c12395ac735f8a2c029a7d8960
Gerhard Tan <gwohau.tan@gmail.com>
2023-07-07 02:45:18 +0800

    Bump version to 1.14.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 var (
-	Number = "1.13.0"
+	Number = "1.14.0"
 	// FRPVersion is the version of FRP used by this program
 	FRPVersion = ""
 	// BuildDate is the day that this program was built
