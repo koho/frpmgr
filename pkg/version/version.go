ecc96efe0475f742c07a4d84ca90c19690d2377e
Gerhard Tan <gwohau.tan@gmail.com>
2025-05-08 15:24:37 +0800

    Bump version to 1.21.1

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	Number = "1.21.0"
+	Number = "1.21.1"
 	// FRPVersion is the version of FRP used by this program
 	FRPVersion = version.Full()
 	// BuildDate is the day that this program was built
